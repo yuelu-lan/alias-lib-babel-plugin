0022bd1b77ad16c3a01e35e0eb01a5740693590a
南月鹿 <942893986@qq.com>
2023-11-19 13:21:53 +0800

    feat: getProComponentPaths 添加容错处理

--- getProComponentPaths.ts
+++ getProComponentPaths.ts
@@ -11,27 +11,31 @@ const getProComponentPaths = () => {
    */
   result.push(path.resolve('./src/.umi/plugin-layout'));
 
-  fs.readdirSync(path.resolve('./node_modules/@ant-design')).forEach(
-    (fileName) => {
-      if (!fileName?.includes('pro')) {
-        return;
-      }
+  try {
+    fs.readdirSync(path.resolve('./node_modules/@ant-design')).forEach(
+      (fileName) => {
+        if (!fileName?.includes('pro')) {
+          return;
+        }
 
-      /**
-       * path.join(__dirname, './node_modules/@ant-design')
-       * 等效于
-       * path.resolve('./node_modules/@ant-design')
-       *
-       *
-       * path.join 只是把接受的 paths 连接在一起
-       * path.resolve 会把 '/' 当成根目录，
-       * 并且，当接受的 paths 没有以 '/' 开头时，会返回一个带当前目录路径的绝对路径。
-       */
-      result.push(
-        path.join(__dirname, `./node_modules/@ant-design/${fileName}`),
-      );
-    },
-  );
+        /**
+         * path.join(__dirname, './node_modules/@ant-design')
+         * 等效于
+         * path.resolve('./node_modules/@ant-design')
+         *
+         *
+         * path.join 只是把接受的 paths 连接在一起
+         * path.resolve 会把 '/' 当成根目录，
+         * 并且，当接受的 paths 没有以 '/' 开头时，会返回一个带当前目录路径的绝对路径。
+         */
+        result.push(
+          path.join(__dirname, `./node_modules/@ant-design/${fileName}`),
+        );
+      },
+    );
+  } catch (error) {
+    console.error('getProComponentPaths error', error);
+  }
 
   return result;
 };
